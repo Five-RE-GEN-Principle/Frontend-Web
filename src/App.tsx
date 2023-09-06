081cdff450b2a6def16603499758252a4666ccc8
Yeonhan Jung <yt4307@gmail.com>
2023-09-06 15:37:20 +0900

    chore: 절대경로 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import HomePage from "@pages/HomePage";
+import Header from "@components/Header";
 
 const App = () => {
   return (
-    <div>
-      <div>test</div>
-    </div>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </>
   );
 };
 
---
+++ src/components/Header.tsx
@@ -0,0 +1,5 @@
+const Header = () => {
+  return <h3>대충 헤더 내용</h3>;
+};
+
+export default Header;
