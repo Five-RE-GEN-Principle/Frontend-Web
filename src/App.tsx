3a4df2c8470c5847d177479c458996037fbd177e
Yeonhan Jung <yt4307@gmail.com>
2023-09-07 17:59:30 +0900

    feat: 제로웨이스트 점수 계산기 페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,15 @@ import HomePage from "@pages/HomePage";
 
 import Layout from "@components/Layout";
 import AboutPage from "@pages/AboutPage";
+import CalculatorPage from "@pages/CalculatorPage";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<HomePage />} />
-        <Route path="about" element={<AboutPage/>} />
+        <Route path="about" element={<AboutPage />} />
+        <Route path="calculator" element={<CalculatorPage />} />
       </Route>
     </Routes>
   );
