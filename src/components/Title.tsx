577f5d0bf71cead13decd5085caac69d07d79c6d
Yeonhan Jung <yt4307@gmail.com>
2023-09-07 17:13:30 +0900

    feat: Title 컴포넌트 추가

---
+++ src/components/Title.tsx
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+interface TitleProps {
+  children: React.ReactNode;
+}
+
+const Title = ({ children }: TitleProps) => {
+  return (
+    <Container>
+      <Text>{children}</Text>
+      <Line />
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Line = styled.div`
+  width: 80px;
+  height: 5px;
+
+  margin-top: 20px;
+  margin-bottom: 20px;
+
+  background-color: #917b56;
+`;
+
+const Text = styled.h1`
+  font-size: 36px;
+  font-weight: 500;
+`;
+
+export default Title;
